1b3c3f9f183123a4a794f76e15b8d67f01474073
andreascoco11 <andreascoco@outlook.com>
2024-02-28 10:22:40 +0100

    Section 3 technically done, still have to check for details and potentially optimize

--- section3.py
+++ section3.py
@@ -1,37 +1,69 @@
 from section1 import Domain
-from section2 import Agent
 import numpy as np
 from itertools import product
+from functools import lru_cache
+import sys
 
+class MDP:
+    def __init__(self, domain):
+        self.domain = domain
 
-def det_proba(domain):
-    probas = np.zeros((domain.g.size, domain.g.size, len(domain.actions)))
-
-    for s, a in product(range(domain.g.size), range(len(domain.actions))):
-        action = domain.actions[a]
-        state = (s // domain.n, s % domain.n)
-        state_prime = domain.dynamic(state, action)
-        s_prime = state_prime[0] * domain.n + state_prime[1]
+    def det_proba(self, s_prime, s, a):
+        if self.domain.dynamic(s, a) == s_prime:
+            return 1
+        else:
+            return 0
         
-        probas[s_prime, s, a] = 1
-    return probas
-
-def sto_proba(domain):
-    probas = np.zeros((domain.g.size, domain.g.size, len(domain.actions)))
-
-    for s, a in product(range(domain.g.size), range(len(domain.actions))):
-        probas[0, s, a] = 0.5
-
-        action = domain.actions[a]
-        state = (s // domain.n, s % domain.n)
-        state_prime = domain.dynamic(state, action)
-        s_prime = state_prime[0] * domain.n + state_prime[1]
-
-        probas[s_prime, s, a] += 0.5
-
-    return probas
+    def sto_proba(self, s_prime, s, a):
+        proba = 0.5 * self.det_proba(s_prime, s, a)
+        if s_prime == (0,0):
+            proba += 0.5
+        return proba
+    
+    def det_rew(self, s, a):
+        return self.domain.g[self.domain.dynamic(s, a)]
+    
+    def sto_rew(self, s, a):
+        return (0.5 * self.domain.g[0, 0]) + 0.5 * self.det_rew(s,a)
+    
+    @lru_cache(maxsize=None)
+    def det_Q_N(self, s, a, N):
+        if N == 0:
+            return 0
+        else:
+            cumul = 0
+            for i, j in product(range(self.domain.n), range(self.domain.m)):
+                s_prime = (i, j)
+                cumul += self.det_proba(s_prime, s, a) * np.max(np.array([self.det_Q_N(s_prime, a_prime, N-1) for a_prime in self.domain.actions]))
+                
+            Q_n = self.det_rew(s, a) + self.domain.discount * cumul
+            return Q_n
+    
+    @lru_cache(maxsize=None)
+    def det_policy(self, N):
+        mu_N = [np.argmax(np.array([self.det_Q_N(s, a, N) for a in self.domain.actions])) for s in product(range(self.domain.n), range(self.domain.m))]
+        return mu_N
+    
+    @lru_cache(maxsize=None)
+    def sto_Q_N(self, s, a, N):
+        if N == 0:
+            return 0
+        else:
+            cumul = 0
+            for i, j in product(range(self.domain.n), range(self.domain.m)):
+                s_prime = (i, j)
+                cumul += self.sto_proba(s_prime, s, a) * np.max(np.array([self.sto_Q_N(s_prime, a_prime, N-1) for a_prime in self.domain.actions]))
+                
+            Q_n = self.sto_rew(s, a) + self.domain.discount * cumul
+            return Q_n
+        
+    @lru_cache(maxsize=None)
+    def sto_policy(self, N):
+        mu_N = [np.argmax(np.array([self.sto_Q_N(s, a, N) for a in self.domain.actions])) for s in product(range(self.domain.n), range(self.domain.m))]
+        return mu_N
 
 if __name__ == "__main__":
+    sys.setrecursionlimit(10**5)
 
     n, m = 5, 5  # Grid size
     g = np.array([[-3,   1,  -5, 0,  19],
@@ -45,13 +77,88 @@ if __name__ == "__main__":
     # Initialize the domain
     domain = Domain(n, m, g, s0, random_state=42)
 
-    det_probas = det_proba(domain)
-    sto_probas = sto_proba(domain)
+    # Initialize MDP
+    mdp = MDP(domain)
 
+    # Finding the deterministic policy such that it doesn't change from one iteration to another
+    n = 0
+    det_current_policy = mdp.det_policy(0)
+    while True:
+        n += 1
+        det_old_policy = det_current_policy
+        det_current_policy = mdp.det_policy(n)
 
-    for s, a in product(range(domain.g.size), range(len(domain.actions))):
-       print(np.sum(det_probas[:,s,a])) # just checking they're all 1
-       print(np.sum(sto_probas[:,s,a]))
+        if det_current_policy == det_old_policy:
+            break
+
+    # print(n, det_current_policy)
+    det_policy = [domain.actions[i] for i in det_current_policy]
+    print("Deterministic policy")
+    for i, j in product(range(domain.n), range(domain.m)):
+        s = (i,j)
+        print("state ", s, "action: ", det_policy[i * domain.n + j])
+
+    # Finding the stochastic policy such that it doesn't change from one iteration to another
+    n = 0
+    sto_current_policy = mdp.sto_policy(0)
+    while True:
+        n += 1
+        sto_old_policy = sto_current_policy
+        sto_current_policy = mdp.sto_policy(n)
+
+        if sto_current_policy == sto_old_policy:
+            break
+
+    # print(n, sto_current_policy)
+    sto_policy = [domain.actions[i] for i in sto_current_policy]
+    print("Stochastic policy")
+    for i, j in product(range(domain.n), range(domain.m)):
+        s = (i,j)
+        print("state ", s, "action: ", sto_policy[i * domain.n + j])
+ 
+    # J function for the deterministic domain
+    @lru_cache(maxsize=None)
+    def det_j_func(domain, state, policy, N):
+        if N == 0:
+            return 0
+        else:
+            j = 0
+            state_index = state[0] * domain.n + state[1]
+            action = policy[state_index]
+            j = domain.det_reward(state, action) + domain.discount * det_j_func(domain, domain.dynamic(state, action), policy, N-1)
+            return j
+        
+    # J function for the deterministic domain
+    @lru_cache(maxsize=None)
+    def sto_j_func(domain, state, policy, N):
+        if N == 0:
+            return 0
+        else:
+            j = 0
+            state_index = state[0] * domain.n + state[1]
+            action = policy[state_index]
+            j = 0.5 * (domain.det_reward(state, action) + domain.discount * sto_j_func(domain, domain.dynamic(state, action), policy, N-1))
+            j += 0.5 * (domain.g[0, 0] + domain.discount * sto_j_func(domain, (0, 0), policy, N-1))
+            return j
+
+    # Value of N we use to estimate J
+    N = 10**4
+
+    # Estimate J for our random policy in deterministic domain:
+    print('Deterministic domain')
+    print("s; J_(mu*)(s)")
+    for i in range(n):
+        for j in range(m):
+            s = (i,j)
+            print(f"({i},{j}); {det_j_func(domain, s, tuple(det_policy), N)}")
+
+    # Estimate J for our random policy in stochastic domain:
+    print('Stochastic domain')
+    print("s; J_(mu*)(s)")
+    for i in range(n):
+        for j in range(m):
+            s = (i,j)
+            print(f"({i},{j}); {sto_j_func(domain, s, tuple(sto_policy), N)}")
 
 
 
