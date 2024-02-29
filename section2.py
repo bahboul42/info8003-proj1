45b07e29c80c123ec96790861c5fdc111f2e5654
andreascoco11 <andreascoco@outlook.com>
2024-02-29 17:11:54 +0100

    Cleaned the section2 code and added comments

--- section2.py
+++ section2.py
@@ -1,38 +1,24 @@
 import numpy as np
 from section1 import Domain
 from functools import lru_cache
-
 import sys
 
-
-
 class Agent:
-    def __init__(self, domain, policy = (0.25, 0.25, 0.25, 0.25)):
-        self.actions = domain.actions # actions the agent can choose from
-        self.policy = policy # probability of choosing each action
+    def __init__(self, domain, policy=(0.25, 0.25, 0.25, 0.25)):
+        self.actions = domain.actions # Actions the agent can execute
+        self.policy = policy # Policy the agent follows
 
-    def chose_action(self, state):
-        """Returns an action given a state"""
-        which_action = np.random.rand()
-        if which_action <= self.policy[0]:
-            return self.actions[0]
-        elif which_action <= self.policy[0] + self.policy[1]:
-            return self.actions[1]
-        elif which_action <= self.policy[0] + self.policy[1] + self.policy[2]:
-            return self.actions[2]
-        else:
-            self.actions[3]
-        
-    
 if __name__ == "__main__":
+    # Increase the recursion limit
     print(sys.setrecursionlimit(10**5))
 
     n, m = 5, 5  # Grid size
+    # Rewards
     g = np.array([[-3,   1,  -5, 0,  19],
                   [ 6,   3,   8, 9,  10],
                   [ 5,  -8,   4, 1,  -8],
                   [ 6,  -9,   4, 19, -5],
-                  [-20, -17, -4, -3,  9],])
+                  [-20, -17, -4, -3,  9]])
 
     s0 = (3, 0)  # Initial state
 
@@ -42,9 +28,11 @@ if __name__ == "__main__":
     # Initialize the agent
     agent = Agent(domain)
 
-    # J function for the deterministic domain
-    @lru_cache(maxsize=None)
+    @lru_cache(maxsize=None) # Use the cache to avoid recomputing several times the same values
     def det_j_func(domain, state, agent, N):
+        """
+        Calculates the value function J_N for a deterministic domain using the agent's policy
+        """
         if N == 0:
             return 0
         else:
@@ -53,9 +41,11 @@ if __name__ == "__main__":
                 j += agent.policy[i] * (domain.det_reward(state, agent.actions[i]) + domain.discount * det_j_func(domain, domain.dynamic(state, agent.actions[i]), agent, N-1))
             return j
         
-    # J function for the stochastic domain
-    @lru_cache(maxsize=None)
+    @lru_cache(maxsize=None) # Use the cache to avoid recomputing several times the same values
     def sto_j_func(domain, state, agent, N):
+        """
+        Calculates the value function J_N for a stochastic domain using the agent's policy
+        """
         if N == 0:
             return 0
         else:
@@ -66,10 +56,10 @@ if __name__ == "__main__":
             j += 0.5 * (domain.g[0, 0] + domain.discount * sto_j_func(domain, (0, 0), agent, N-1))
             return j
         
-    # Value of N we use to estimate J
+    # Value of N used to estimate J
     N = 10**4
 
-    # Estimate J for our random policy in deterministic domain:
+    # Estimate J for the random policy in the deterministic domain
     print('Deterministic domain')
     print("s; J(s)")
     for i in range(n):
@@ -77,7 +67,7 @@ if __name__ == "__main__":
             s = (i,j)
             print(f"({i},{j}); {det_j_func(domain, s, agent, N)}")
 
-    # Estimate J for our random policy in stochastic domain:
+    # Estimate J for the random policy in the stochastic domain
     print('Stochastic domain')
     print("s; J(s)")
     for i in range(n):
@@ -85,6 +75,6 @@ if __name__ == "__main__":
             s = (i,j)
             print(f"({i},{j}); {sto_j_func(domain, s, agent, N)}")
 
+    # Estimate the accuracy of the estimation
     print("Estimation accuracy:")
     print((domain.discount**(N))*np.max(domain.g)/(1-domain.discount))
-
