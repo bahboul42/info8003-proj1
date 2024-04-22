a54bf086f799a9ca257642c007b4207520f685c4
andreascoco11 <andreascoco@outlook.com>
2024-04-22 09:35:57 +0200

    final version ddpg

--- src/p3/ddpg.py
+++ src/p3/ddpg.py
@@ -12,24 +12,27 @@ torch.cuda.manual_seed(seed_value) if torch.cuda.is_available() else None
 np.random.seed(seed_value)
 
 # Setting the parameter values
-lr_actor = 0.001
+lr_actor = 0.0001
 lr_critic = 0.001
-tau = 0.01
+tau = 0.001
 n_transitions = 1000
-n_episodes = 10000
-noise_std = 0.5
-buffer_size = 50000
+n_episodes = 100000
+noise_std = 0.3 # 0.5 for the simple pendulum
+buffer_size = 1000000
 batch_size = 64
+gamma = 0.99
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class Actor(nn.Module):
     def __init__(self, state_dim, action_dim, double=False):
         super().__init__()
-        self.fc1 = nn.Linear(state_dim, 128)
-        self.fc2 = nn.Linear(128, 256)
-        self.fc3 = nn.Linear(256, 128)
-        self.fc4 = nn.Linear(128, action_dim)
+        self.fc1 = nn.Linear(state_dim, 400)
+        self.fc2 = nn.Linear(400, 300)
+        self.fc3 = nn.Linear(300, action_dim)
+
+        torch.nn.init.uniform_(self.fc3.weight, -0.003, 0.003)
+        torch.nn.init.uniform_(self.fc3.bias, -0.003, 0.003)
 
         self.tanh = nn.Tanh()
         self.relu = nn.ReLU()
@@ -41,8 +44,7 @@ class Actor(nn.Module):
     def forward(self, state):
         x = self.relu(self.fc1(state))
         x = self.relu(self.fc2(x))
-        x = self.relu(self.fc3(x))
-        x = self.tanh(self.fc4(x))
+        x = self.tanh(self.fc3(x))
         if not self.double:
             x = 3 * x # Action space is [-3, 3] for simple pendulum
         return x
@@ -71,23 +73,24 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     def __init__(self, state_dim, action_dim, double=False):
         super().__init__()
-        self.fc1 = nn.Linear(state_dim + action_dim, 128)
-        self.fc2 = nn.Linear(128, 256)
-        self.fc3 = nn.Linear(256, 128)
-        self.fc4 = nn.Linear(128, 1)
+        self.fc1 = nn.Linear(state_dim, 400-action_dim)
+        self.fc2 = nn.Linear(400, 300)
+        self.fc3 = nn.Linear(300, 1)
+
+        torch.nn.init.uniform_(self.fc3.weight, -0.003, 0.003)
+        torch.nn.init.uniform_(self.fc3.bias, -0.003, 0.003)
 
         self.relu = nn.ReLU()
 
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=lr_critic)
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=lr_critic, weight_decay = 0.01)
 
         self.loss_fct = nn.MSELoss()
 
     def forward(self, state, action):
-        x = torch.cat([state, action], 1)
-        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc1(state))
+        x = torch.cat([x, action], dim=1)
         x = self.relu(self.fc2(x))
-        x = self.relu(self.fc3(x))
-        x = self.fc4(x)
+        x = self.fc3(x)
         if not self.double:
             x = self.relu(x) # No negative rewards for simple pendulum
         return x
@@ -130,7 +133,7 @@ class ReplayBuffer():
             dones.append(done)
         return torch.cat(states, dim=0), torch.cat(actions, dim=0), torch.tensor(rewards, dtype=torch.float32).to(device), torch.tensor(next_states, dtype=torch.float32).to(device), torch.tensor(dones, dtype=torch.float32).to(device)
 
-def ddpg(env, n_episodes, n_transitions, noise_std, buffer_size, batch_size, tau, double=False):
+def ddpg(env, n_episodes, n_transitions, noise_std, buffer_size, batch_size, tau, gamma, double=False):
     buffer = ReplayBuffer(buffer_size)
 
     state_dim = env.observation_space.shape[0]
@@ -142,6 +145,12 @@ def ddpg(env, n_episodes, n_transitions, noise_std, buffer_size, batch_size, tau
     target_actor = deepcopy(actor).to(device)
     target_critic = deepcopy(critic).to(device)
 
+    # We don't need to compute their gradients
+    for param in target_actor.parameters():
+            param.requires_grad = True
+    for param in target_critic.parameters():
+            param.requires_grad = True
+
     hasConverged = False
 
     for episode in range(n_episodes):
@@ -151,18 +160,15 @@ def ddpg(env, n_episodes, n_transitions, noise_std, buffer_size, batch_size, tau
         state, _ = env.reset()
         noise_std = noise_std * (0.1 ** (1 / n_episodes))
 
-        # total_rew = 0
-
         for _ in range(n_transitions):
             state = torch.tensor(state, dtype=torch.float32).to(device)
 
-            action = actor(state).detach() + noise_std * torch.randn(action_dim).to(device)
+            action = actor(state).detach()
+            action = action + noise_std * torch.randn(action_dim).to(device)
             action = torch.clamp(action, -3, 3)
 
             next_state, reward, terminated, truncated, _ = env.step(action.cpu().numpy())
             done = 1 if terminated else 0
-
-            # total_rew += reward
 
             buffer.add(state, action, reward, next_state, done)
 
@@ -172,7 +178,7 @@ def ddpg(env, n_episodes, n_transitions, noise_std, buffer_size, batch_size, tau
                 rewards = rewards.unsqueeze(dim=-1)
                 dones = dones.unsqueeze(dim=-1)
 
-                y = rewards + target_critic(next_states.to(device), target_actor(next_states.to(device))) * (1 - dones)
+                y = rewards + gamma * target_critic(next_states.to(device), target_actor(next_states.to(device))) * (1 - dones)
 
                 critic.train(states.to(device), actions.to(device), y.to(device))
 
@@ -187,32 +193,50 @@ def ddpg(env, n_episodes, n_transitions, noise_std, buffer_size, batch_size, tau
             state = next_state
 
         if (episode + 1) % 100 == 0:
-            torch.save(actor.state_dict(), "actor.pth")
-            torch.save(critic.state_dict(), "critic.pth")
+            if double:
+                torch.save(actor.state_dict(), f"dactor.pth")
+                torch.save(critic.state_dict(), "dcritic.pth")
+            else:
+                torch.save(actor.state_dict(), "actor.pth")
+                torch.save(critic.state_dict(), "critic.pth")
 
             print(f"Episode {episode + 1}/{n_episodes}")
 
-            state, _ = env.reset()
-            total_rew = 0
-            for _ in range(n_transitions):
-                state = torch.tensor(state, dtype=torch.float32).to(device)
-                action = actor(state).detach()
-                next_state, reward, terminated, _, _ = env.step(action.cpu().numpy())
-                total_rew += reward
-                if terminated:
-                    break
-                state = next_state
+            avg_rew = 0
+            avg_steps = 0
+            for _ in range(100):
+                state, _ = env.reset()
+                for timestep in range(1, n_transitions+1):
+                    with torch.no_grad():
+                      state = torch.tensor(state, dtype=torch.float32).to(device)
+                      action = actor(state).detach()
+                      next_state, reward, terminated, truncated, _ = env.step(action.cpu().numpy())
+                      avg_rew += reward/100
+                      if terminated or truncated:
+                          avg_steps += timestep/100
+                          break
+                      state = next_state
 
-            print(f"Total reward: {total_rew}")
-            if total_rew == 1000:
+            print(f"Average reward: {avg_rew}")
+            print(f"Average number of time steps: {avg_steps}")
+            if not double and avg_rew >= 999:
                 hasConverged = True
                 print('converged')
 
     return actor, critic
 
 def main():
+    '''print('Simple inverted pendulum')
+    # Initialize the environment
     env = gym.make("InvertedPendulum-v4")
-    actor, critic = ddpg(env, n_episodes, n_transitions, noise_std, buffer_size, batch_size, tau)
+    # Apply the algorithm
+    actor, critic = ddpg(env, n_episodes, n_transitions, noise_std, buffer_size, batch_size, tau, double=False)
+    env.close()'''
+    print('Double inverted pendulum')
+    # Initialize the environment
+    env = gym.make("InvertedDoublePendulum-v4")
+    # Apply the algorithm
+    actor, critic = ddpg(env, n_episodes, n_transitions, noise_std, buffer_size, batch_size, tau, double=True)
     env.close()
 
 if __name__ == "__main__":
